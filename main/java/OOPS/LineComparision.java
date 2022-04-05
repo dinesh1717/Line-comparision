475fdff7a83ccf0a5b34326f635f7a469ca16ad6
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-05 07:54:34 +0530

    check equality of 2 lines

--- main/java/OOPS/LineComparision.java
+++ main/java/OOPS/LineComparision.java
@@ -7,6 +7,17 @@ public class LineComparision {
         return length;
     }
 
+    public static int LengthEquality(double length1, double lenght2) {
+        if (length1 == lenght2) {
+            System.out.println("Both the line are equal in size");
+        } else if (length1 > lenght2) {
+            System.out.println("First line is bigger in size");
+        } else {
+            System.out.println("Second line is bigger in size");
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to line comparision program");
@@ -16,7 +27,16 @@ public class LineComparision {
         double y1 = scanner.nextDouble();
         double x2 = scanner.nextDouble();
         double y2 = scanner.nextDouble();
-        double length = lengthCalc(x1, y1, x2, y2);
-        System.out.println(length);
+        double length1 = lengthCalc(x1, y1, x2, y2);
+
+        System.out.println("Enter a1, b1 and a2, b2 for line 2");
+        double a1 = scanner.nextDouble();
+        double b1 = scanner.nextDouble();
+        double a2 = scanner.nextDouble();
+        double b2 = scanner.nextDouble();
+        double length2 = lengthCalc(a1, b1, a2, b2);
+
+        int length = LengthEquality(length1, length2);
+
     }
 }
